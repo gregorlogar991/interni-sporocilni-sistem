2787ec8f0e598e31e6d1671fdec94a31b38ab1ce
Mark Kuhar <kuharmark@gmail.com>
2015-04-21 15:43:03 +0200

    u
    dmnnd

--- login.php
+++ login.php
@@ -9,15 +9,15 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
-    <title>Signin Template for Bootstrap</title>
+    <title>Vpis</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
@@ -34,6 +34,49 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style type="text/css">
+
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #eee;
+      }
+      .form-signin {
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin .checkbox {
+        font-weight: normal;
+      }
+      .form-signin .form-control {
+        position: relative;
+        height: auto;
+        -webkit-box-sizing: border-box;
+           -moz-box-sizing: border-box;
+                box-sizing: border-box;
+        padding: 10px;
+        font-size: 16px;
+      }
+      .form-signin .form-control:focus {
+        z-index: 2;
+      }
+      .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+      .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+      }
+
+    </style>
   </head>
 
   <body>
@@ -49,6 +92,8 @@
         <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Sign in</button>
       </form>
 
+
+
     </div> <!-- /container -->
 
 
@@ -61,10 +106,10 @@
 <?php
   
   if(isset($_POST['submit'])){
-    $con = mysqli_connect("localhost","root","","sporocilni_sistem") or die("Error " . mysqli_error($link));
+    $con = mysqli_connect("localhost","root","","sporocilni_sistem") or die("Error " . mysqli_error($con));
 
-    $user = $_POST['user'];
-    $geslo = $_POST['geslo'];
+    $user = mysqli_real_escape_string($_POST['user']);
+    $geslo = sha1($_POST['geslo']);
 
     if($_POST['user'] == "" || $_POST['geslo'] == ""){
       echo 'izpolnite podatke';
