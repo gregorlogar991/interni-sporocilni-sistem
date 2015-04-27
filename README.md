ccc9c1bce88776dfda5e0ebc3138f55a183df940
markkuhar <kuharmark@gmail.com>
2015-04-27 15:54:26 +0200

    Za prjavt v sistem je narjen

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Pr registraciji dodaj enkripcijo gesla pa mysqli_real_escape_string
 
-
+$_SESSION['id'] <-- sm se upiše id uporabnika k je upisan
 
 ----------------------------- 
 Mi smo fantastični trije.
--- index.php
+++ index.php
@@ -1,1 +1,69 @@
-//kos
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+      <link rel="stylesheet" type="text/css" href="loginRegistracija.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    <title>Interni sporocilni sistem</title>
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="signin.css" rel="stylesheet">
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" method="POST" action="">
+        <h2 class="form-signin-heading" align="center">Interni sistem</h2>
+        <label for="inputEmail" class="sr-only">Uporabnisko ime</label>
+        <input type="text" id="inputEmail" class="form-control" name="user" placeholder="Uporabnisko ime">
+        <label for="inputPassword" class="sr-only">Geslo</label>
+        <input type="password" id="inputPassword" name="geslo" class="form-control" placeholder="Geslo">
+        <?php
+          
+          if(isset($_POST['submit'])){
+            $con = mysqli_connect("localhost","root","","sporocilni_sistem") or die("Error " . mysqli_error($link));
+
+            $user = mysqli_real_escape_string($con, $_POST['user']);
+            $geslo = mysqli_real_escape_string($con, $_POST['geslo']);
+
+            if($_POST['user'] == "" || $_POST['geslo'] == ""){
+              echo '<font color="red">Izpolnite VSE podatke</font>';
+            }
+            else{
+              $sql = "select ID_uporabnika from uporabnik where uporabnisko_ime ='$user' and geslo ='$geslo'";
+              $select = mysqli_query($con, $sql);
+              if(mysqli_num_rows($select) == 1){
+                session_start();
+                $_SESSION['id'] = mysqli_fetch_row($select)[0];
+                header('Location: profil.php');
+              }
+              else{
+                echo '<font color="red" align="center">Napacni podatki!</font>';
+              }
+            }
+          }
+        ?>
+        <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Vpis</button>
+        <a class="btn btn-lg btn-success btn-block" href="registracija.php">Registracija</a>
+      </form>
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+
