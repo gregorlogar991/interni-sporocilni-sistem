8f93235b6b462222771675b2439be66482fa8c8b
markkuhar <kuharmark@gmail.com>
2015-05-02 21:40:09 +0200

    baza s skupinam

--- sporocilni_sistem.sql
+++ sporocilni_sistem.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Gostitelj: 127.0.0.1
--- Čas nastanka: 02. maj 2015 ob 15.43
+-- Čas nastanka: 02. maj 2015 ob 21.39
 -- Različica strežnika: 5.6.20
 -- Različica PHP: 5.5.15
 
@@ -19,6 +19,57 @@ SET time_zone = "+00:00";
 --
 -- Zbirka podatkov: `sporocilni_sistem`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `skupina`
+--
+
+CREATE TABLE IF NOT EXISTS `skupina` (
+`id_skupine` int(11) NOT NULL,
+  `naslov` varchar(20) NOT NULL,
+  `id_lastnika` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Odloži podatke za tabelo `skupina`
+--
+
+INSERT INTO `skupina` (`id_skupine`, `naslov`, `id_lastnika`) VALUES
+(3, 'Sluzba', 1),
+(4, 'Prijatelji', 1),
+(5, 'Druzba iz morja', 1),
+(6, 'Nogomet grupa', 1),
+(9, 'Burazi', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `skupinauporabnik`
+--
+
+CREATE TABLE IF NOT EXISTS `skupinauporabnik` (
+`id_skupinauporabnik` int(11) NOT NULL,
+  `id_lastnika` int(11) NOT NULL,
+  `id_skupine` int(11) NOT NULL,
+  `id_dodanega` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Odloži podatke za tabelo `skupinauporabnik`
+--
+
+INSERT INTO `skupinauporabnik` (`id_skupinauporabnik`, `id_lastnika`, `id_skupine`, `id_dodanega`) VALUES
+(6, 1, 4, 4),
+(7, 1, 4, 8),
+(8, 1, 6, 3),
+(9, 1, 5, 6),
+(10, 1, 4, 2),
+(11, 1, 5, 4),
+(12, 1, 4, 10),
+(13, 1, 3, 7),
+(14, 1, 3, 7);
 
 -- --------------------------------------------------------
 
@@ -69,15 +120,14 @@ CREATE TABLE IF NOT EXISTS `uporabnik` (
 --
 
 INSERT INTO `uporabnik` (`ID_uporabnika`, `ime`, `priimek`, `uporabnisko_ime`, `geslo`, `zadnja_prijava`) VALUES
-(1, 'Domen', 'Kos', 'dkos', 'kos', '2015-05-01 19:35:06'),
+(1, 'Domen', 'Kos', 'dkos', 'kos', '2015-05-02 21:11:56'),
 (2, 'Mark', 'Kuhar', 'kmark', 'kuhar', '2015-05-01 19:53:31'),
 (3, 'Grega', 'Logar', 'glogar', 'logar', NULL),
 (4, 'Patrik', 'Istinic', 'pistinic', 'istinic', NULL),
-(5, 'Klemen', 'Kogovsek', 'kkogovsek', 'kogovsek', NULL),
+(5, 'Klemen', 'Kogovsek', 'kkogovsek', 'kogovsek', '2015-05-02 21:10:21'),
 (6, 'Tilen', 'Jesenko', 'tjesenko', 'jesenko', NULL),
 (7, 'Janez', 'Kosir', 'jkosir', 'kosir', '2015-04-29 17:10:29'),
 (8, 'Ziga', 'Mali', 'zmali', 'mali', NULL),
-(9, '', '', '', '', NULL),
 (10, 'Janez', 'Novak', 'jnovak', 'novak', NULL),
 (11, 'Marko', 'Gruden', 'mgruden', 'gruden', NULL),
 (12, 'Kristjan', 'Grm', 'kgrm', 'grm', NULL);
@@ -85,6 +135,18 @@ INSERT INTO `uporabnik` (`ID_uporabnika`, `ime`, `priimek`, `uporabnisko_ime`, `
 --
 -- Indeksi zavrženih tabel
 --
+
+--
+-- Indeksi tabele `skupina`
+--
+ALTER TABLE `skupina`
+ ADD PRIMARY KEY (`id_skupine`);
+
+--
+-- Indeksi tabele `skupinauporabnik`
+--
+ALTER TABLE `skupinauporabnik`
+ ADD PRIMARY KEY (`id_skupinauporabnik`);
 
 --
 -- Indeksi tabele `transakcija`
@@ -102,6 +164,16 @@ ALTER TABLE `uporabnik`
 -- AUTO_INCREMENT zavrženih tabel
 --
 
+--
+-- AUTO_INCREMENT tabele `skupina`
+--
+ALTER TABLE `skupina`
+MODIFY `id_skupine` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT tabele `skupinauporabnik`
+--
+ALTER TABLE `skupinauporabnik`
+MODIFY `id_skupinauporabnik` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT tabele `transakcija`
 --
